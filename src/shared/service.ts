d31466efc45260731744944275ad82914c0f8d9e
GopinathSurendran <sgopinath.sms@gmail.com>
2021-06-09 11:54:06 -0400

    added ci cd

---
+++ src/shared/service.ts
@@ -0,0 +1,17 @@
+import axios from "axios"
+
+export const getTopStories = async (limit = 10) => {
+    const response = await axios.get('topstories.json')
+    if (response.status === 200) {
+        return response.data.slice(0, limit);
+    }
+    return null;
+}
+
+export const getItemDetails = async (itemId: number) => {
+    const response = await axios.get(`item/${itemId}.json`)
+    if (response.status === 200) {
+        return response.data;
+    }
+    return null;
+}
